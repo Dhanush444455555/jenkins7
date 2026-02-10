0e740d706af7f148cc2191381089e22a93327e57
Dhanush <aishudhanush984555@gmail.com>
2026-02-10 22:58:11 +0530

    Update App.java

--- App.java
+++ App.java
@@ -1,7 +1,9 @@
 package com.example;
 
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello! This build is triggered by Jenkins.");
+        System.out.println("Hello from Jenkins Maven Build!");
+        System.out.println("This JAR was generated automatically.");
     }
 }
